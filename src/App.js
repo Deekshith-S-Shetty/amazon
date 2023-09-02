5f9e551a20f68399ac785932bea43c109347c511
Deekshith Shetty <deekshithshetty698@gmail.com>
2023-09-02 13:34:41 +0530

    Setup Header component and update index css to set 0 margin and padding

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import Home from "./components/Home";
+import Header from "./components/Header";
 import { Routes, Route } from "react-router-dom";
 import "./App.css";
 
@@ -6,7 +7,16 @@ function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route
+          exact
+          path="/"
+          element={
+            <>
+              <Home />
+              <Header />
+            </>
+          }
+        />
       </Routes>
     </div>
   );
