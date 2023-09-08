f6d42c48261fdc67a3a649439859ee44881076b8
Deekshith Shetty <deekshithshetty698@gmail.com>
2023-09-08 20:18:43 +0530

    Wrap our app to use Data layer for state managements

--- src/index.js
+++ src/index.js
@@ -4,12 +4,16 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
+import reducer, { initialState } from "./components/DataLayer/reducer";
+import { StateProvider } from "./components/DataLayer/StateProvider";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <StateProvider initialState={initialState} reducer={reducer}>
+        <App />
+      </StateProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
