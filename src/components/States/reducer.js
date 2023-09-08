f297af12a6a6d179611899c14b477a1c8443260d
Deekshith Shetty <deekshithshetty698@gmail.com>
2023-09-08 20:12:54 +0530

    Initialize reducer for the application

---
+++ src/components/States/reducer.js
@@ -0,0 +1,15 @@
+// All the states are declared in the initialState
+export const initialState = {
+  basket: [],
+  user: null,
+};
+
+// Performs the given action on the data and returns the result
+const reducer = (state, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
+
+export default reducer;
