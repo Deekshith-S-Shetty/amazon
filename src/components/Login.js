9453b569c9c27991bb87c00eb2cb90e077e8507e
Deekshith Shetty <deekshithshetty698@gmail.com>
2023-09-15 20:21:03 +0530

    Add login amd register options with email and password

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,5 +1,38 @@
+import { Link } from "react-router-dom";
+
 function Login() {
-  return <div>Login</div>;
+  return (
+    <div className="login">
+      <Link to="/">
+        <img
+          className="login-logo"
+          src="/images/amazonLogo.png"
+          alt="amazon logo"
+        />
+      </Link>
+      <div className="login-container">
+        <h1>Sign-in</h1>
+        <form>
+          <h5>E-mail</h5>
+          <input type="email" />
+
+          <h5>Password</h5>
+          <input type="password" />
+
+          <button type="submit" className="login-btn">
+            Sign In
+          </button>
+        </form>
+        <p>
+          By signing-in you agree to the amazon Conditions of Use & Sale. Please
+          see our Privacy Notice, our Cookies Notice and our Interest-Based Ads
+          Notice.
+        </p>
+
+        <button className="register-btn">Create your Amazon Account</button>
+      </div>
+    </div>
+  );
 }
 
 export default Login;
