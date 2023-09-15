5f2aeb25ea5fb69c159067ef2602ca0bb6b0d5b1
Deekshith Shetty <deekshithshetty698@gmail.com>
2023-09-15 21:01:44 +0530

    Add state variables for email and password

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,7 +1,10 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import "./Login.css";
 
 function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   return (
     <div className="login">
       <Link to="/">
@@ -15,10 +18,18 @@ function Login() {
         <h1>Sign-in</h1>
         <form>
           <h5>E-mail</h5>
-          <input type="email" />
+          <input
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
 
           <h5>Password</h5>
-          <input type="password" />
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
 
           <button type="submit" className="login-btn">
             Sign In
