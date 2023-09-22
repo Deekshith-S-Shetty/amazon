eca3eee015572014336dd37a2b1f17b6e92916a8
Deekshith Shetty <deekshithshetty698@gmail.com>
2023-09-22 19:02:30 +0530

    Update Firebase file to newer version and supported features

--- src/components/DataLayer/Firebase.js
+++ src/components/DataLayer/Firebase.js
@@ -1,4 +1,6 @@
-import firebase from "firebase";
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth";
+import "firebase/compat/firestore";
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
@@ -11,8 +13,8 @@ const firebaseConfig = {
   measurementId: "G-72CYF46Z4F",
 };
 
-const firebaseApp = firebase.initializeApp(firebaseConfig);
-const db = firebaseApp.firestore();
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
 const auth = firebase.auth();
 
 export { db, auth };
