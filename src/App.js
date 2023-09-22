68435565cb7d52d759f87396cc1e524dc9273144
Deekshith Shetty <deekshithshetty698@gmail.com>
2023-09-22 19:41:46 +0530

    Set the user when authentication change or login/logout occurs

--- src/App.js
+++ src/App.js
@@ -1,11 +1,35 @@
+import { useEffect } from "react";
+import { Routes, Route } from "react-router-dom";
+import { auth } from "./components/DataLayer/Firebase";
 import Checkout from "./components/Checkout";
 import Home from "./components/Home";
 import Header from "./components/Header";
 import Login from "./components/Login";
-import { Routes, Route } from "react-router-dom";
 import "./App.css";
+import { useStateValue } from "./components/DataLayer/StateProvider";
 
 function App() {
+  const [{}, dispatch] = useStateValue();
+
+  useEffect(() => {
+    console.log("printing");
+    auth.onAuthStateChanged((authUser) => {
+      console.log("The user is : ", authUser);
+
+      if (authUser) {
+        dispatch({
+          type: "SET_USER",
+          user: authUser,
+        });
+      } else {
+        dispatch({
+          type: "SET_user",
+          user: null,
+        });
+      }
+    });
+  }, []);
+
   return (
     <div className="App">
       <Routes>
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
-import "./Login.css";
 import { auth } from "./DataLayer/Firebase";
+import "./Login.css";
 
 function Login() {
   const [email, setEmail] = useState("");
