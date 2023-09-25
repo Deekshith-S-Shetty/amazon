c169fbd94cd93f5ccc48650800fa838d05fd320a
Deekshith Shetty <deekshithshetty698@gmail.com>
2023-09-25 21:57:09 +0530

    Develop Checkout Page with product details

--- src/components/Checkout.js
+++ src/components/Checkout.js
@@ -1,5 +1,40 @@
+import { useStateValue } from "./DataLayer/StateProvider";
+import CheckoutProduct from "./CheckoutProduct";
+import SubTotal from "./SubTotal";
+import "./Checkout.css";
+
 function Checkout() {
-  return <div>Checkout</div>;
+  const [{ basket, user }, dispatch] = useStateValue();
+  return (
+    <div className="checkout">
+      <div className="checkout-left">
+        <img
+          className="checkout-banner"
+          src="./images/checkoutPage.jpg"
+          alt="Checkout Banner"
+        />
+        <div>
+          <h3>Hello, {user?.email}</h3>
+          <h2 className="checkout-title">Your Shopping Basket</h2>
+
+          {basket.map((item) => {
+            return (
+              <CheckoutProduct
+                id={item.id}
+                title={item.title}
+                image={item.image}
+                price={item.price}
+                rating={item.rating}
+              />
+            );
+          })}
+        </div>
+      </div>
+      <div className="checkout-right">
+        <span>Total amounts come here</span>
+      </div>
+    </div>
+  );
 }
 
 export default Checkout;
--- src/components/CheckoutProduct.js
+++ src/components/CheckoutProduct.js
@@ -1,36 +1,34 @@
 import { useStateValue } from "./DataLayer/StateProvider";
-import "./CheckoutProduct.css";
 
-function CheckoutProduct({ id, image, title, price, rating, hideButton }) {
-  const [{ basket }, dispatch] = useStateValue();
+function CheckoutProduct({ id, image, title, price, rating }) {
+  const [{}, dispatch] = useStateValue();
 
-  const removeFromBasket = () => {
-    dispatch({
-      type: "removeFromBasket",
-      id: id,
-    });
-  };
+  const removeFromBasket = () => {};
 
   return (
-    <div className="checkoutProduct">
-      <img className="checkoutProduct__image" src={image} alt="Product" />
+    <div className="checkout-product">
+      <img className="checkout-product-img" src={image} alt="Product" />
 
-      <div className="checkoutProduct__info">
-        <p className="checkoutProduct__title">{title}</p>
-        <p className="checkoutProduct__price">
+      <div className="product-info">
+        <p className="product-title">{title}</p>
+        <p className="product-price">
           <small>$</small>
           <strong>{price}</strong>
         </p>
-        <div className="checkoutProduct__rating">
+        <div className="product-rating">
           {Array(rating)
             .fill()
-            .map((_, i) => (
-              <p>🌟</p>
-            ))}
+            .map((value, index) => {
+              return <p key={index}>🌟</p>;
+            })}
         </div>
-        {!hideButton && (
-          <button onClick={removeFromBasket}>Remove from Basket</button>
-        )}
+        <button
+          id="remove-from-basket-btn"
+          className="btn"
+          onClick={removeFromBasket}
+        >
+          Remove From Basket
+        </button>
       </div>
     </div>
   );
