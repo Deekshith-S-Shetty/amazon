542fdc555cd6da15a6af2360b72a6453409317c5
Deekshith Shetty <deekshithshetty698@gmail.com>
2023-09-25 21:47:56 +0530

    Setup Reducer with all the actions and functions

--- src/components/DataLayer/reducer.js
+++ src/components/DataLayer/reducer.js
@@ -4,6 +4,10 @@ export const initialState = {
   user: null,
 };
 
+export const getBasketTotal = (basket) => {
+  basket.reduce((amount, item) => item.price + amount, 0);
+};
+
 // Performs the given action on the data and returns the result
 const reducer = (state, action) => {
   switch (action.type) {
@@ -12,11 +16,30 @@ const reducer = (state, action) => {
         ...state,
         basket: [...state.basket, action.item],
       };
+
+    case "removeFromBasket":
+      const index = state.basket.findIndex(
+        (basketItem) => basketItem.id === action.id
+      );
+      let newBasket = [...state.basket];
+      if (index >= 0) {
+        newBasket.splice(index, 1);
+      } else {
+        console.warn(
+          `Cant remove product (id: ${action.id}) as its not in basket!`
+        );
+      }
+      return {
+        ...state,
+        basket: newBasket,
+      };
+
     case "SET_USER":
       return {
         ...state,
         user: action.user,
       };
+
     default:
       return state;
   }
